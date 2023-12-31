ecca8757201652c5dd0450d928383b04ce6bdb40
Peter Bloem <git@peterbloem.nl>
2023-12-31 14:12:49 +0100

    ...

--- cramming/backend/__init__.py
+++ cramming/backend/__init__.py
@@ -1,6 +1,6 @@
 """This module implements interfaces to the various backends."""
 
 from .prepare_backend import load_backend
-from torch_default import _load_optimizer
+from .torch_default import _load_optimizer
 
 __all__ = ["load_backend"]
