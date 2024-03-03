dacbec5073a436d24b41897e594394de09fb1824
Peter Bloem <git@peterbloem.nl>
2024-03-03 13:02:14 +0100

    ...

--- pretrain.py
+++ pretrain.py
@@ -597,6 +597,10 @@ def main_training_process(cfg, setup):
     # Launch training
     for step, batch in enumerate(dataloader, initial_step + 1):
 
+        print(batch.keys())
+        print(batch)
+        exit()
+
         if rmix > 0.0:
             b, l = batch[0].size()
             k = int(rmix * b)
