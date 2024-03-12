8589128f240c283738c9cf114782a24b35ea0c1d
Peter Bloem <git@peterbloem.nl>
2024-03-12 22:14:10 +0100

    ...

--- pretrain.py
+++ pretrain.py
@@ -586,7 +586,8 @@ def main_training_process(cfg, setup):
 
             # -- reuse the optimizer from the UP training
 
-    model_engine.optimizer.betas = cfg.up.betas
+    if cfg.uop.reset_betas:
+        model_engine.optimizer.betas = cfg.up.betas
 
     model_engine.train(cfg.train.pretrain_in_train_mode)
     stats = defaultdict(list)
