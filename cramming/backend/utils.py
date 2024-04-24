7bd94782cb2e2f995176270be7c745bdca533fba
Peter Bloem <git@peterbloem.nl>
2024-04-24 08:00:00 +0200

    ...

--- cramming/backend/utils.py
+++ cramming/backend/utils.py
@@ -333,7 +333,7 @@ class InfiniteDataLoader(torch.utils.data.DataLoader):
             try:
                 batch = next(self.dataset_iterator)
             except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e: # HF server error
-                if rec < MAX_REC
+                if rec < MAX_REC:
                     warnings.warn(f'Encountered HTTP error. Waiting {SLEEP_TIME} seconds and trying again ({rec}/{MAX_REC}).')
                     time.sleep(SLEEP_TIME)
                     return self.__next__(rec + 1)
